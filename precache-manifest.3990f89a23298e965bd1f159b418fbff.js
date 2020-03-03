18b9ca8567e66236cf4a31b2266a3169b0b64725
yonimor88 <yonimor88@gmail.com>
2020-03-03 20:53:26 +0200

    Updates

---
+++ precache-manifest.3990f89a23298e965bd1f159b418fbff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c213beaf15197139af0aca29173fbb",
+    "url": "/smart-house/index.html"
+  },
+  {
+    "revision": "089e9c1e663eb4f57d27",
+    "url": "/smart-house/static/css/main.0e70d848.chunk.css"
+  },
+  {
+    "revision": "0fac40088adadae649ad",
+    "url": "/smart-house/static/js/2.fb73b374.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/smart-house/static/js/2.fb73b374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089e9c1e663eb4f57d27",
+    "url": "/smart-house/static/js/main.86bb3fee.chunk.js"
+  },
+  {
+    "revision": "2ab9715d27d3d9846ba3",
+    "url": "/smart-house/static/js/runtime-main.e0a1f3a3.js"
+  },
+  {
+    "revision": "83a69d24a8d6f96331da6fdfeeb75e84",
+    "url": "/smart-house/static/media/Homepage_icon.83a69d24.png"
+  },
+  {
+    "revision": "7365bc178d1272b6685536a95f47ef2f",
+    "url": "/smart-house/static/media/smart-home-logo.7365bc17.png"
+  }
+]);
